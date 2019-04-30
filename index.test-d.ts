8a53304cad8e72ab594cb3f78274810e06800040
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-30 11:07:31 +0200

    Require Node.js 8, add TypeScript definition

---
+++ index.test-d.ts
@@ -0,0 +1,8 @@
+import {expectType} from 'tsd';
+import lcid = require('.');
+import lcidJson = require('./lcid.json');
+
+expectType<number>(lcid.to('nb_NO'));
+expectType<string>(lcid.from(1044));
+expectType<{readonly [key: string]: number}>(lcid.all);
+expectType<{readonly [key: string]: number}>(lcidJson);

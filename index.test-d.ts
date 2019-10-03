d1218e6d8db18c764485957ad8f5497372e98523
Tye Peck <tye@novatasolutions.com.au>
2019-10-03 09:57:49 +0800

    fixed missing tests

--- index.test-d.ts
+++ index.test-d.ts
@@ -4,5 +4,5 @@ import lcidJson = require('./lcid.json');
 
 expectType<number>(lcid.to('nb_NO'));
 expectType<string>(lcid.from(1044));
-expectType<{readonly [key: string]: number}>(lcid.all);
-expectType<{readonly [key: string]: number}>(lcidJson);
+expectType<{readonly [key: string]: string}>(lcid.all);
+expectType<{readonly [key: string]: string}>(lcidJson);

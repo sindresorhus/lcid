01381807bdbafea57d193c14eda82905082ccc97
Ricard Mallafre <rikitzzz@gmail.com>
2023-01-17 05:39:30 +0100

    Fix TypeScript types (#13)

--- index.test-d.ts
+++ index.test-d.ts
@@ -2,7 +2,7 @@ import {expectType} from 'tsd';
 import lcid = require('.');
 import lcidJson = require('./lcid.json');
 
-expectType<number>(lcid.to('nb_NO'));
-expectType<string>(lcid.from(1044));
+expectType<number | undefined>(lcid.to('nb_NO'));
+expectType<string | undefined>(lcid.from(1044));
 expectType<{readonly [key: string]: string}>(lcid.all);
 expectType<{readonly [key: string]: string}>(lcidJson);
